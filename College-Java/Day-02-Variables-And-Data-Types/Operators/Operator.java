8f598ede303d96e7626a5a9bb997da1f5ff65d96
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2026-02-12 12:00:00 +0530

    Initialized Day-02-variables-And-Data-Types Folder in College-Java

---
+++ College-Java/Day-02-Variables-And-Data-Types/Operators/Operator.java
@@ -0,0 +1,9 @@
+public class Operator {
+    public static void main(String[] args) {
+        int num1 = 10,num2 = 2;
+        int sum = num1 + num2;
+        System.out.println("1st Number :- " + num1);
+        System.out.println("2nd Number :- " + num2);
+        System.out.println("Sum Of Both "+ num1 +" & "+ num2 +" :- " + sum);
+    }
+}
